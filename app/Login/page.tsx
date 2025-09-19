36a729395e7fe15e3b6ebe6ecfdf7141dd9907ef
Dhruvsgit234 <dhruvlekhi3@gmail.com>
2025-09-19 18:44:23 +0530

    added shiz

---
+++ app/Login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LoginPage() {
+
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+   
+    </div>
+  );
+}
