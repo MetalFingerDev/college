1d91a62e400ba2bf78f30024b3f9cffa318fa9c5
Metal Mask <dev.prajwalp@gmail.com>
2025-09-25 14:15:29 +0530

    convexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,21 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 
+export const metadata: Metadata = {
+	title: "college",
+	description: "kaal age",
+};
+
+/*
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
 if (!convexUrl) {
 	throw new Error("NEXT_PUBLIC_CONVEX_URL is not set in environment variables");
 }
+const convex = new ConvexReactClient(convexUrl); */
+
+//  Placeholder URL for development
+const convexUrl =
+	process.env.NEXT_PUBLIC_CONVEX_URL || "https://placeholder.convex.cloud";
 const convex = new ConvexReactClient(convexUrl);
 
 export default function RootLayout({
